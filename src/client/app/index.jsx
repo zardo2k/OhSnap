9ac4597003a290783a793c4c038fc18da6131659
Van Co <van@nutanix.com>
2016-08-27 08:40:14 -0700

    Webpack dev server is now working

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -6,7 +6,7 @@ class App extends React.Component {
   render () {
     return (
       <div>
-        <p> Hello React!</p>
+        <p> Hello WodGoodr!</p>
         <AwesomeComponent />
       </div>
     );
