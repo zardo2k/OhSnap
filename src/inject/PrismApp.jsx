da5187c351f7ed35486f3144f25eeed9dadf7c4b
Van Co <van@nutanix.com>
2016-08-30 11:49:49 -0700

    popup styling and injection

--- src/inject/PrismApp.jsx
+++ src/inject/PrismApp.jsx
@@ -14,14 +14,27 @@ class PrismApp extends React.Component {
 
     _.bindAll(this, 'onKeyPress');
   }
+
   componentWillMount() {
     console.log('Install Global Event Handler');
     $('body').on('keypress', this.onKeyPress);
+    this.startMonitorSupportCasePopup();
   }
 
   componentWillUnmount() {
     console.log('Uninstall Global Event Handler');
     $('body').off('keypress', this.onKeyPress);
+  }
+
+  startMonitorSupportCasePopup() {
+    var intervalId = setInterval(function() {
+      if ($('#popupSupportCase').length &&
+          $('.preview-wrapper').length === 0) {
+        var el = $('<div class="preview-wrapper">');
+        $('.n-modal-body').prepend(el);
+        $('#popupSupportCase').addClass('ohSnap');
+      }
+    }, 2000);
   }
 
   onKeyPress(event) {
